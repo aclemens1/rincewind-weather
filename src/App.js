29d7ae7e77944e79853862b7d027a93e233bd03c
Alex Clemens <alexsydneynsw@gmail.com>
2024-04-08 16:30:08 +1000

    cleanup and basic error handling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import Filter from "./components/filter"
 
 function App() {
 
+  const mapsCoAPIKey = "66133c6b97c68881283499kcn082424"
+  const pirateWeatherAPIKey = "gH9MnPxi72TolKeaAv0DxZhWVmzRqSV7"
+
   const [weather, setWeather] = useState(null)
   const [address, setAddress] = useState("")
   const [loading, setLoading] = useState(false)
 
   const getData = async (address) => {
     setLoading(true)
-    fetch("https://geocode.maps.co/search?q=" + address + "&api_key=66133c6b97c68881283499kcn082424")
-      .then(response => response.json())
+    fetch("https://geocode.maps.co/search?q=" + address + "&api_key=" + mapsCoAPIKey)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("We encountered an error while we were trying to get coordinates for this location.")
+          return null
+        }
+        return response.json()
+      })
       .then((data) => {
         if (data && data.length > 0 && data[0].lat && data[0].lon) {
-          fetch("https://api.pirateweather.net/forecast/gH9MnPxi72TolKeaAv0DxZhWVmzRqSV7/" + data[0].lat + "," + data[0].lon)
-            .then((response) => response.json())
+          fetch("https://api.pirateweather.net/forecast/" + pirateWeatherAPIKey + "/" + data[0].lat + "," + data[0].lon)
+            .then((response) => {
+              if (!response.ok) {
+                console.log("We encountered an error while we were trying to get weather for the coordinates of this location.")
+                return null
+              }
+              return response.json()
+            })
             .then((data) => {
               setWeather(data)
               setLoading(false)
@@ -36,7 +51,7 @@ function App() {
       <button onClick={handleClick} disabled={loading}>What's the weather today?</button>
       <br />
       {loading && <div>loading...</div>}
-      {!loading && <Card city={address} weather={weather} />}
+      {!loading && <Card weather={weather} />}
     </>
   )
 
